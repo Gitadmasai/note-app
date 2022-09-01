d03e9431dc9e7f16bfd640cd0a7f6f7555e2ee24
Rahul Kumar <97456833+Gitadmasai@users.noreply.github.com>
2022-09-01 16:06:21 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,52 @@
+let submit=document.querySelector(".submit");
+let notesElem=document.querySelector('.notes');
+let title=document.querySelector("#text");
+let desc=document.querySelector('#desc');
+let dat=document.querySelector('#dat');
+let notes=JSON.parse(localStorage.getItem("notes"));
+if(notes){
+    notes.forEach(element => {
+        addNotes(element)
+    });
+}
+submit.addEventListener("click", (e)=>{
+    e.preventDefault();
+    addNotes()
+})
+function addNotes(obj) {
+    let card=document.createElement("div");
+    card.classList.add("card");
+    let titleval=title.value;
+    let descVal=desc.value;
+    let datVal=dat.value;
+    if(obj){
+        titleval=obj.title;
+        descVal=obj.desc;
+        datVal=obj.dat;
+    }
+    if(titleval){
+        card.innerHTML=`<h3>${titleval}</h3>
+                                    <p class="ptag">${descVal}</p>
+                                    <h2>${datVal}</h2>
+                             <button class="del">Delete</button>`;
+        notesElem.appendChild(card);
+        updateLs()
+    }
+    let del=card.querySelector(".del");
+    del.addEventListener('click', ()=>{
+        card.remove();
+        updateLs();
+    })
+}
+function updateLs() {
+    let card=document.querySelectorAll(".card");
+    let arr=[];
+    card.forEach(element => {
+        arr.push({
+            title:element.children[0].innerText,
+            desc:element.children[1].innerText,
+            dat:element.children[2].innerText
+        })
+    });
+    localStorage.setItem("notes", JSON.stringify(arr));
+}
